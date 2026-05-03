057d59fc12d4d281695ee264fc771a9fab5cb0ac
lz20r <zhiyaonaiara@gmail.com>
2026-05-03 16:12:07 +0200

    feat: agregar rutas para el panel de control y gestión de servidores en la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Footer } from '@/sections/Footer';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from '@/pages/HomePage';
 import { CommandsPage } from '@/pages/CommandsPage';
+import { DashboardPage } from '@/pages/DashboardPage';
+import { ServerPage } from '@/pages/ServerPage';
+import { ModuleConfigPage } from '@/pages/ModuleConfigPage';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/commands" element={<CommandsPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard/servers/:guildId" element={<ServerPage />} />
+        <Route path="/dashboard/servers/:guildId/modules/:moduleId" element={<ModuleConfigPage />} />
       </Routes>
       <Footer />
     </div>
